1b3227a528314e5ca94fa9e268488d86467a887a
NIKHILPESALA1 <nikhilpesala4@gmail.com>
2025-12-17 14:55:58 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,14 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Nikhil Pesala | Computer Science Engineer",
-  description: "DevOps, Cloud, AI/ML Engineer",
+  title: "Nikhil Pesala | Portfolio",
+  description: "DevOps, Cloud, AI Engineer Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <main className="max-w-5xl mx-auto px-6">
-          {children}
-        </main>
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
